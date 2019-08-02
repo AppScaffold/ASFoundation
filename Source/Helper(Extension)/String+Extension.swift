88f1720047c877ce8867e6578560bef06b3d9068
jesse <square.zhao.wei@gmail.com>
2019-08-02 19:14:30 +0800

    Fixed warning.

--- Source/Helper(Extension)/String+Extension.swift
+++ Source/Helper(Extension)/String+Extension.swift
@@ -9,19 +9,21 @@
 import Foundation
 import CommonCrypto
 
-extension String {
+public extension String {
     var MD5: String? {
         let length = Int(CC_MD5_DIGEST_LENGTH)
         guard let data = self.data(using: String.Encoding.utf8) else { return nil }
-        let hash = data.withUnsafeBytes { (bytes: UnsafePointer<Data>) -> [UInt8] in
+        
+        let hash = data.withUnsafeBytes { (bytes) -> [UInt8] in
+            let bufferPointer = bytes.bindMemory(to: UInt8.self)
             var hash: [UInt8] = [UInt8](repeating: 0, count: Int(CC_MD5_DIGEST_LENGTH))
-            CC_MD5(bytes, CC_LONG(data.count), &hash)
+            CC_MD5(bufferPointer.baseAddress!, CC_LONG(data.count), &hash)
             return hash
         }
         return (0..<length).map { String(format: "%02x", hash[$0]) }.joined()
     }
     
-    public var date: Date? {
+    var date: Date? {
         let timestampFormat: String = "yyyy-MM-dd'T'HH:mm:ss"
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = timestampFormat
@@ -30,7 +32,7 @@ extension String {
 }
 
 // Helper of substring
-extension String {
+public extension String {
     // [1] or [n]
     subscript (i: Int) -> String {
         return self[i ..< i + 1]
