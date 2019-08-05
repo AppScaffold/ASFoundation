d3a2519876319009c6416db6914c553bbdf01c50
jesse <square.zhao.wei@gmail.com>
2019-08-05 17:49:01 +0800

    Established building project.

---
+++ ASFoundation/ASFoundation/ASFoundation.h
@@ -0,0 +1,19 @@
+//
+//  ASFoundation.h
+//  ASFoundation
+//
+//  Created by jesse on 2019/7/29.
+//  Copyright © 2019 jesse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ASFoundation.
+FOUNDATION_EXPORT double ASFoundationVersionNumber;
+
+//! Project version string for ASFoundation.
+FOUNDATION_EXPORT const unsigned char ASFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ASFoundation/PublicHeader.h>
+
+
